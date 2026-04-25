158b6a2249ce00e3f77eaf2ff65171d644bb2c93
Steak-Fish <85029813+Steak-Fish@users.noreply.github.com>
2026-04-25 14:06:09 -0500

    Update edition names and latest version in ConfigHandler

--- src/main/java/net/coreprotect/config/ConfigHandler.java
+++ src/main/java/net/coreprotect/config/ConfigHandler.java
@@ -51,11 +51,11 @@ public class ConfigHandler extends Queue {
     public static final int EDITION_VERSION = 2;
     public static final String EDITION_BRANCH = VersionUtils.getBranch();
     public static final String EDITION_NAME = VersionUtils.getPluginName();
-    public static final String COMMUNITY_EDITION = "Community Edition";
+    public static final String COMMUNITY_EDITION = "SteakFish Edition";
     public static final String JAVA_VERSION = "11.0";
     public static final String MINECRAFT_VERSION = "1.16";
     public static final String PATCH_VERSION = "23.2";
-    public static final String LATEST_VERSION = "26.1";
+    public static final String LATEST_VERSION = "26.1.2";
     public static String path = "plugins/CoreProtect/";
     public static String sqlite = "database.db";
     public static String host = "127.0.0.1";
